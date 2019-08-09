3b378121534c23e8303e37e422ae229006dc3b5e
Tanuj22 <agarwal.5@iitj.ac.in>
2019-08-09 22:49:32 +0530

    added a temp homepage

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Layout from '../components/Layout.js';
+
+class VotingIndex extends Component{
+    
+    render(){
+        return(
+            <Layout>
+                <h2>Hello</h2>
+            </Layout>
+        );
+    }
+}
+
+export default VotingIndex;
