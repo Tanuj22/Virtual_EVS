a4bae63a3d86e4dd97a9e44404344b5834dc8028
Tanuj22 <agarwal.5@iitj.ac.in>
2019-08-09 22:47:55 +0530

    added Layout and Header components

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes.js';
+
+export default () =>{
+    return(
+        <Menu style = {{ marginTop : '10px' }}>
+            <Link route="/">
+                <a className="item">Virtual EVS</a>
+            </Link>
+
+            <Menu.Menu position = "right">
+                <Link route="/register">
+                    <a className="item">Register</a>
+                </Link>
+
+                <Link route="/">
+                    <a className="item">Vote</a>
+                </Link>
+                <Link route="/">
+                    <a className="item">Admin Panel</a>
+                </Link>
+            </Menu.Menu>
+        </Menu>
+    );
+}
