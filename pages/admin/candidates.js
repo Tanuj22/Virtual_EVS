8db103f33949c746608cd3cc996f1dc2baff8400
Tanuj22 <agarwal.5@iitj.ac.in>
2019-08-16 12:23:13 +0530

    changed admin/voters and candidates

--- pages/admin/candidates.js
+++ pages/admin/candidates.js
@@ -2,25 +2,14 @@ import React, {Component} from 'react';
 import Layout from '../../components/Layout';
 import { Button,Table } from 'semantic-ui-react'
 import voting from '../../ethereum/voting';
-import web3 from '../../ethereum/web3';
-import { Router,Link } from '../../routes.js';
+import { Link } from '../../routes.js';
 import RequestRow from '../../components/requestCandidateRow'; 
 
 
 class VoterAdmin extends Component{
     static async getInitialProps(){
-        // const accounts = await web3.eth.getAccounts();
-        // const hasAccess = await voting.methods.isManager().send({
-        //     from : accounts[0]
-        // });
-        // if(!hasAccess)
-        // {
-        //     Router.push('/admin');
-        // }
-        // else{
             const registeredCandidates = await voting.methods.getCandidates().call();
             return {registeredCandidates};
-        // }
     }
 
     CandidateList(){
--- pages/admin/voters.js
+++ pages/admin/voters.js
@@ -2,25 +2,14 @@ import React, {Component} from 'react';
 import Layout from '../../components/Layout';
 import { Button, Table } from 'semantic-ui-react'
 import voting from '../../ethereum/voting';
-import web3 from '../../ethereum/web3';
-import { Router, Link } from '../../routes.js';
+import { Link } from '../../routes.js';
 import RequestRow from '../../components/requestVoterRow'; 
 
 
 class VoterAdmin extends Component{
     static async getInitialProps(){
-        const accounts = await web3.eth.getAccounts();
-        const hasAccess = await voting.methods.isManager().send({
-            from : accounts[0]
-        });
-        if(!hasAccess)
-        {
-            Router.push('/admin');
-        }
-        else{
-            const registeredVoters = await voting.methods.getVoters().call();
-            return {registeredVoters};
-         }
+        const registeredVoters = await voting.methods.getVoters().call();
+        return {registeredVoters};
     }
 
     VotersList(){
